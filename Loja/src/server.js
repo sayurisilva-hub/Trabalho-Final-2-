1883fb2048695c6a5fdcdc236afdd0bb48acfdcf
sayurisilva-hub <sayuri.silva@estudante.ifms.edu.br>
2025-11-19 21:17:28 -0400

    Update server.js

--- Loja/src/server.js
+++ Loja/src/server.js
@@ -11,3 +11,13 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
